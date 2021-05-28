89cea1511837cf7466ab3bbeffce36376139c43c
Personman000 <saad_syed_ali@hotmail.com>
2021-05-28 19:00:30 -0400

    Form updated

--- s/autofill.js
+++ s/autofill.js
@@ -7,75 +7,90 @@ window.onload = function () {
         input.onchange = populateEmail;
     }
 
-    populateList("#subject_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/subject_lines.txt');
-    populateList("#body_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/bodies.txt');
     populateList("#mp_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/mps.txt');
-    populateList("#tip_list_1", "li", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/tips1.txt');
-    populateList("#tip_list_2", "li", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/tips2.txt');
+    populateList("#subject_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/subject_lines.txt');
+    populateList("#purpose_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/purposes.txt');
+    populateList("#connection_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/connections.txt');
+    populateList("#issue_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/issues.txt');
+    populateList("#connection_tip_list", "li", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/connection_tips.txt');
     populateListCheckboxes("#call_to_action_list", "input", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/calls_to_action.txt');
+    populateList("#closing_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/closings.txt');
+    populateList("#review_tip_list", "li", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/review_tips.txt');
 
     // integration functionality
+    var step_2_name = document.getElementById("user-input");
     var step_2_button = document.getElementById("button");
     var step_2_results = document.getElementById("results-table");
 
-    if(step_2_button && step_2_results) {
+    // If step 2 items exist (i.e. we are on the squarespace site)
+    if(step_2_button && step_2_results && step_2_name) {
 
         // On step 2 button click
         step_2_button.onclick = function() {
 
             // Populate mp name using step 2 input
-            var step_2_name = document.getElementById("user-input");
-            document.getElementById("mp").value = step_2_name.value;
+            document.forms["autofillForm"]["mp"].value = step_2_name.value; // Autopop mp name
+            document.forms["autofillForm"]["email_target"].value = "";      // Empty mp email so that populateEmail() can populate it
             populateEmail();
         }
 
         // Load acknowledge menu when mousing over form (janky workaround)
-        document.getElementById("autofillForm").onmouseenter = function() {
+        document.getElementById("acknowledge").onmouseenter = function () {
+            if(document.getElementById("acknowledge") !== document.activeElement){
 
-            // Populate acknowledgement list using step 2 results
-            var step_2_values = step_2_results.getElementsByTagName("td");
-            var step_2_value;
+                // Populate acknowledgement list using step 2 results
+                var step_2_values = step_2_results.getElementsByTagName("td");
+                var step_2_value;
 
-            var has_red = false;
-            var has_green = false;
+                var has_red = false;
+                var has_green = false;
 
-            console.log(step_2_values);
-            console.log(step_2_values.length);
+                //console.log(step_2_values);
+                //console.log(step_2_values.length);
 
-            // Loop through results list
-            for(var i = 0; i < step_2_values.length; i++) {
+                // Loop through results list
+                for(var i = 0; i < step_2_values.length; i++) {
 
-                step_2_value = step_2_values[i];
+                    step_2_value = step_2_values[i];
 
-                // Check if red values exist
-                if(step_2_value.style.backgroundColor == "darkred") {
-                    has_red = true;
+                    // Check if red values exist
+                    if(step_2_value.style.borderColor == "darkred") {
+                        has_red = true;
+                    }
+                    // Check if green values exist
+                    if(step_2_value.style.borderColor == "green") {
+                        has_green = true;
+                    }
                 }
-                // Check if green values exist
-                else if(step_2_value.style.backgroundColor == "green") {
-                    has_green = true;
+
+
+                var ack_sub = document.getElementById("acknowledgement_subtitle");
+
+                // Populate acknowledgement list using the colors found
+                if(has_red == true && has_green == true) {
+                    populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/both_acknowledgements.txt');
+                    ack_sub.innerHTML = "Your MP has taken some action to support Palestine and some action to support Israel’s apartheid. Thank them and demand better.";
                 }
+                else if(has_red == true) {
+                    populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/red_acknowledgements.txt');
+                    ack_sub.innerHTML = "Your MP has taken action to support Israel’s apartheid. Demand better from them.";
+                }
+                else if(has_green == true) {
+                    populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/green_acknowledgements.txt');
+                    ack_sub.innerHTML = "In Step 2, you learned that your MP has taken action to support Palestine. Thank them.";
+                }
+                else {
+                    populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/neither_acknowledgements.txt');
+                    ack_sub.innerHTML = "We could not find any information about your MP’s stance on Palestine. But you can speak to their party’s stance and actions.";
+                }
+            }
+        })
 
-                console.log(step_2_value);
-                console.log(step_2_value.style.backgroundColor);
-            }
-
-            console.log(has_red);
-            console.log(has_green);
-
-            // Populate acknowledgement list using the colors found
-            if(has_red == true && has_green == true) {
-                populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/double_acknowledgements.txt');
-            }
-            else if(has_red == true) {
-                populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/red_acknowledgements.txt');
-            }
-            else if(has_green == true) {
-                populateList("#acknowledge_list", "option", 'https://raw.githubusercontent.com/Personman000/email-populater/master/lists/green_acknowledgements.txt');
-            }
-            else {
-                document.getElementById("acknowledge_list").innerHTML = "";
-            }
+        // On mp name change, update Step 2
+        document.getElementById("mp").onchange = function() {
+            step_2_name.value = document.getElementById("mp").value;    // Plug in mp name
+            step_2_button.click();                                      // Run stance checker
+            populateEmail();
         }
     }
 
@@ -155,15 +170,31 @@ function populateEmail() {
     var name = document.forms["autofillForm"]["name"];
     var email_source = document.forms["autofillForm"]["email_source"];
     var mp = document.forms["autofillForm"]["mp"];
+    var email_target = document.forms["autofillForm"]["email_target"];
+    var subject = document.forms["autofillForm"]["subject"];
+    var purpose = document.forms["autofillForm"]["purpose"];
+    var connection = document.forms["autofillForm"]["connection"];
+    var issue = document.forms["autofillForm"]["issue"];
+    var acknowledge = document.forms["autofillForm"]["acknowledge"];
+    var cta_list = document.getElementById("call_to_action_list");
+    var closing = document.forms["autofillForm"]["closing"];
+
     var email_text = document.forms["autofillForm"]["email_text"];
-    var body = document.forms["autofillForm"]["body"];
-    var acknowledge = document.forms["autofillForm"]["acknowledge"]
-    var cta_list = document.getElementById("call_to_action_list")
     
 
     // Get MP name and email
     var mp_name = mp.value;
-    var email_target = mp.value.replace(/\s/g, '');
+    if(mp_name.value != "") {
+
+        var mp_email = getMPEmail();
+
+        if(mp_email != false) {
+            email_target.value = mp_email.toLowerCase();;
+        }
+        else {
+            email_target.value = "";
+        }
+    }
 
     // Populate email text
     var email_text = document.getElementById("email_text");
@@ -189,14 +220,25 @@ function populateEmail() {
         email_text.value += "my name is " + name.value + ".";
     }
 
-    // Populate email body
-    if (body.value != "") {
+    // Populate purpose
+    if (purpose.value != "") {
 
-        email_text.value += " " + body.value;
+        email_text.value += " " + purpose.value;
+    }
+
+    // Populate connection
+    if (connection.value != "") {
+
+        email_text.value += "\n\n" + connection.value;
+    }
+
+    // Populate issue
+    if (issue.value != "") {
+
+        email_text.value += "\n\n" + issue.value;
     }
 
     // Populate acknowledgement
-    // Populate email body
     if (acknowledge.value != "") {
 
         email_text.value += "\n\n" + acknowledge.value;
@@ -220,6 +262,12 @@ function populateEmail() {
         }
     }
 
+
+    // Populate closing
+    if (closing.value != "") {
+
+        email_text.value += "\n\n" + closing.value;
+    }
 }
 
 
@@ -229,6 +277,7 @@ function submitForm() {
     // Get form data
     var name = document.forms["autofillForm"]["name"].value;
     var email_source = document.forms["autofillForm"]["email_source"].value;
+    var email_target = document.forms["autofillForm"]["email_target"].value;
     var mp = document.forms["autofillForm"]["mp"].value;
     var subject = document.forms["autofillForm"]["subject"].value;
 
@@ -255,7 +304,7 @@ function submitForm() {
 
     // Send email
     var email_text = document.getElementById("email_text").value;
-    window.open('mailto:'+mp+'@canada.gov?subject='+subject+'&body='+email_text);
+    window.open(`mailto:${email_target}?subject=${subject}&body=${email_text}`);
 }
 
 
@@ -265,9 +314,6 @@ function showHideFromLabel() {
     var element = document.getElementById(label.getAttribute("for"));
     var display = element.style.display;
 
-    
-    
-
     // If invisible, turn visible
     if(display == "none")
     {
@@ -276,5 +322,28 @@ function showHideFromLabel() {
     // If visible, turn invisible
     else {
         element.style.display = "none";
+    }
+}
+
+function getMPEmail() {
+    var name = document.getElementById("mp").value;
+
+    // Get JSON
+    var request = `https://represent.opennorth.ca/representatives/house-of-commons/?name=${name.replaceAll(" ", "%20")}`;
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", request, false);
+    xmlHttp.send();
+
+    // Parse JSON
+    let list = (JSON.parse(xmlHttp.responseText)).objects;
+
+    // Get email item from JSON
+    var item = list[0];
+    if(typeof item !== "undefined") {
+        return(item.email);
+    }
+    else {
+        return false;
     }
 }
